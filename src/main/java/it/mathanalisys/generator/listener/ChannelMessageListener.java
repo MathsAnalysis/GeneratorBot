81ab27e26ede203007658f99b6bad40f2d82219c
Carlo <112445885+MathsAnalysis@users.noreply.github.com>
2023-09-07 19:36:50 +0200

    added
    added system for remove message in the channel and send log in other channel

--- src/main/java/it/mathanalisys/generator/listener/ChannelMessageListener.java
+++ src/main/java/it/mathanalisys/generator/listener/ChannelMessageListener.java
@@ -1,18 +1,24 @@
 package it.mathanalisys.generator.listener;
 
 import it.mathanalisys.generator.Generator;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bson.Document;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ChannelMessageListener extends ListenerAdapter {
 
@@ -23,10 +29,8 @@ public class ChannelMessageListener extends ListenerAdapter {
         Member member = event.getMember();
         if (member == null) return;
 
-
-        if (event.getChannel().getId().equals("1149294603284000809") && !event.getMessage().getAttachments().isEmpty()) {
+        if (event.getChannel().getId().equals("1149215610333577229") && !event.getMessage().getAttachments().isEmpty()) {
             Generator.get().getDatabaseManager().getFiles().drop();
-
 
             Role role = event.getGuild().getRoleById("1149214454844772372");
             if (Generator.get().hasRoleOrHigher(member, role)) return;
@@ -47,6 +51,22 @@ public class ChannelMessageListener extends ListenerAdapter {
                     Generator.get().getDatabaseManager().getFiles().insertMany(docsToInsert);
                     System.out.println(docsToInsert.size() + " account inseriti nel database.");
 
+                    TextChannel otherChannel = event.getJDA().getTextChannelById("1149397104851165304");
+                    EmbedBuilder builder = new EmbedBuilder();
+                    builder.setColor(Color.BLUE);
+                    builder.setTitle("Log Account");
+                    builder.appendDescription("\n");
+                    builder.appendDescription("Total accounts: " + docsToInsert.size() + "\n");
+                    builder.appendDescription("Type: Basic\n");
+                    builder.appendDescription("\n");
+                    builder.setFooter(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()));
+
+
+                    if (otherChannel != null) {
+                        otherChannel.sendMessageEmbeds(builder.build()).queue();
+                    }
+
+                    event.getMessage().delete().queueAfter(30, TimeUnit.SECONDS);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -54,6 +74,7 @@ public class ChannelMessageListener extends ListenerAdapter {
                 t.printStackTrace();
                 return null;
             }));
+
         }
     }
 }
--- src/main/java/it/mathanalisys/generator/listener/ChannelMessagePlusListener.java
+++ src/main/java/it/mathanalisys/generator/listener/ChannelMessagePlusListener.java
@@ -1,18 +1,24 @@
 package it.mathanalisys.generator.listener;
 
 import it.mathanalisys.generator.Generator;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bson.Document;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ChannelMessagePlusListener extends ListenerAdapter {
 
@@ -45,6 +51,22 @@ public class ChannelMessagePlusListener extends ListenerAdapter {
                     Generator.get().getDatabaseManager().getFilesPlus().insertMany(docsToInsert);
                     System.out.println(docsToInsert.size() + " account inseriti nel database.");
 
+                    TextChannel otherChannel = event.getJDA().getTextChannelById("1149397104851165304");
+                    EmbedBuilder builder = new EmbedBuilder();
+                    builder.setColor(Color.BLUE);
+                    builder.setTitle("Log Account");
+                    builder.appendDescription("\n");
+                    builder.appendDescription("Total accounts: " + docsToInsert.size() + "\n");
+                    builder.appendDescription("Type: Basic+\n");
+                    builder.appendDescription("\n");
+                    builder.setFooter(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()));
+
+
+                    if (otherChannel != null) {
+                        otherChannel.sendMessageEmbeds(builder.build()).queue();
+                    }
+
+                    event.getMessage().delete().queueAfter(30, TimeUnit.SECONDS);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
