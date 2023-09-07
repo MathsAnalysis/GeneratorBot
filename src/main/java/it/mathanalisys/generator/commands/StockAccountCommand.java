86fef3f1a8454542aa5a68931f7bfa22b72a5b3f
Carlo <112445885+MathsAnalysis@users.noreply.github.com>
2023-09-07 12:51:15 +0200

    Update StockAccountCommand.java

--- src/main/java/it/mathanalisys/generator/commands/StockAccountCommand.java
+++ src/main/java/it/mathanalisys/generator/commands/StockAccountCommand.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 import java.util.concurrent.TimeUnit;
@@ -28,21 +29,25 @@ public class StockAccountCommand extends ListenerAdapter {
                 long countFilesPlus = Generator.get().getDatabaseManager().getFilesPlus().countDocuments();
                 long countFilesPlusPlus = Generator.get().getDatabaseManager().getFilesPlusPlus().countDocuments();
 
-                EmbedBuilder builder = new EmbedBuilder();
-
-                builder.setTitle("Accounts Available")
-                        .appendDescription(
-                                "\n" +
-                                        "Basic Account: " + countFiles + "\n" +
-                                        "Basic+ Account: " + countFilesPlus + "\n" +
-                                        "Basic++ Account: " + countFilesPlusPlus + "\n"
-                                        + "\n"
-                        ).setColor(Color.CYAN);
+                EmbedBuilder builder = getBuilder(countFiles, countFilesPlus, countFilesPlusPlus);
 
                 event.deferReply().queue();
                 event.getHook().sendMessageEmbeds(builder.build()).queue(message -> message.delete().queueAfter(20, TimeUnit.SECONDS));
             }
         }
+    }
+
+    @NotNull
+    private static EmbedBuilder getBuilder(long countFiles, long countFilesPlus, long countFilesPlusPlus) {
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setTitle("Accounts Available");
+        builder.appendDescription("\n");
+        builder.appendDescription("Basic Account: " + countFiles + "\n");
+        builder.appendDescription("Basic+ Account: " + countFilesPlus + "\n");
+        builder.appendDescription("Basic++ Account: " + countFilesPlusPlus + "\n");
+        builder.appendDescription("\n");
+        builder.setColor(Color.CYAN);
+        return builder;
     }
 
 }
